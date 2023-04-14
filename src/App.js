d2d97a79cdd4dd937c1a0d701c7bb6f5828267c5
saketsingh2211 <sakets.ug20.ee@nitp.ac.in>
2023-04-14 21:39:02 +0530

    Ready to use

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {                   //function based component
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//whatever written under return() is jsx.
+//jsx is completely html who's wearing the crown of javascript.  90-95% html  
